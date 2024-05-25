14efb1198f72c4268d0db405710ce539c74c47fe
turnernator1 <turn0438@flinders.edu.au>
2024-05-25 23:17:08 +0930

    initial comming

---
+++ app/layout.tsx
@@ -0,0 +1,28 @@
+import type { Metadata } from "next";
+import { Nunito } from "next/font/google";
+import "./globals.css";
+import Navbar from "./components/navbar/Navbar";
+import RegisterModal from "./components/modals/RegisterModal";
+
+const font = Nunito ({ subsets: ["latin"] });
+
+export const metadata: Metadata = {
+  title: "AirBNB | Home",
+  description: "Book your next stay with AirBnB",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en">
+      <body className={font.className}>
+        <RegisterModal />
+        <Navbar />
+        {children}
+        </body>
+    </html>
+  );
+}
